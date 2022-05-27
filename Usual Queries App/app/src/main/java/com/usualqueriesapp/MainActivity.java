5abd29b0d0024def04f57ae2c5d9aee59b1e0415
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2022-05-27 10:11:26 +0530

    Update MainActivity.java

--- Usual Queries App/app/src/main/java/com/usualqueriesapp/MainActivity.java
+++ Usual Queries App/app/src/main/java/com/usualqueriesapp/MainActivity.java
@@ -1,63 +1,56 @@
 package com.usualqueriesapp;
-
 import android.app.*;
 import android.content.*;
 import android.os.*;
 import android.view.*;
 import android.webkit.*;
-
-public class MainActivity extends Activity
-{
-	
-	private WebView mywebView;
+public class MainActivity extends Activity {
+    private WebView mywebView;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		mywebView=(WebView) findViewById(R.id.webview);
-		mywebView.setWebViewClient(new WebViewClient());
-		mywebView.loadUrl("https://usualqueries.blogspot.com/");
-		WebSettings webSettings=mywebView.getSettings();
-		mywebView.getSettings().setDomStorageEnabled(true);
-		mywebView.getSettings().setDatabaseEnabled(true);
-		webSettings.setJavaScriptEnabled(true);
-		mywebView.setWebChromeClient(new WebChromeClient() {
-			@Override
-			public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-				return super.onJsAlert(view, url, message, result);
-			}
-		});
+        setContentView(R.layout.main);
+        mywebView = (WebView) findViewById(R.id.webview);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("https://usualqueries.blogspot.com/"); // Change this URL with your website
+        WebSettings webSettings = mywebView.getSettings();
+        mywebView.getSettings().setDomStorageEnabled(true);
+        mywebView.getSettings().setDatabaseEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.setWebChromeClient(new WebChromeClient() {
+            @Override // This one is to enable alerts and prompts
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) { 
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
     }
-	public class mywebClient extends WebViewClient{
+    public class mywebClient extends WebViewClient {
         @Override
-		public boolean shouldOverrideUrlLoading(WebView view,String url){
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
         }
     }
-	@Override
-    public void onBackPressed(){
-        if(mywebView.canGoBack()) {
+    @Override // This is for pressing back. Can be changed if you want something else to happen after pressing back
+    public void onBackPressed() {
+        if (mywebView.canGoBack()) {
             mywebView.goBack();
+        } else {
+            super.onBackPressed();
         }
-		else{
-			super.onBackPressed();
-		}
-	}
-private class Splash extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splashscreen);
-
-        int secondsDelayed = 3;
-        new Handler().postDelayed(new Runnable() {
-                public void run() {
-					startActivity(new Intent(Splash.this, MainActivity.class));
-					finish();
-                }
-			}, secondsDelayed * 1000);
     }
-}
+    private class Splash extends Activity {
+        @Override // The splashscreen override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.splashscreen); // Contents to be changed in layout file
+            int secondsDelayed = 3; // Time dor which the splashscreen will be displayed. Can be changed 
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(Splash.this, MainActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 1000);
+        }
+    }
 }
